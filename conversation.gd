d03071ece611a7a3a421921af5107dd927cc5cd7
Creativity Shark <94210249+CreativityShark@users.noreply.github.com>
2024-06-24 23:11:08 -0500

    Add conversation object and dialogue object

--- conversation.gd
+++ conversation.gd
@@ -1,11 +1,37 @@
+class_name Conversation
 extends Node
 
 
-# Called when the node enters the scene tree for the first time.
+var dialogues = []
+@export var file_name: String
+
+
 func _ready():
-	pass # Replace with function body.
+	if file_name:
+		load_file(file_name)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+# Pre: filepath leads to a txt file formatted like so:
+#	Line 1: character name
+#	Line 2: path to character image
+#	Line 3: text to display
+func load_from_filepath(filepath: String):
+	assert(FileAccess.file_exists(filepath))
+	var file = FileAccess.open(filepath, FileAccess.READ)
+	while file.get_position() < file.get_length():
+		var dialogue = Dialogue.new()
+		dialogue.name = file.get_line()
+		dialogue.image = load(file.get_line())
+		dialogue.text = file.get_line()
+		assert(file.get_error() == 0)
+		dialogues.push_front(dialogue)
+
+
+func load_file(file_name: String):
+	load_from_filepath("res://dialogue/" + file_name + ".txt")
+
+
+class Dialogue:
+	var name: String
+	var image: Texture2D
+	var text: String
