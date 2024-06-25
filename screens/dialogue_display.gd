8c1d54e217fefcb7c7b01a90723652ab1e8c0a8b
Creativity Shark <94210249+CreativityShark@users.noreply.github.com>
2024-06-25 00:12:09 -0500

    Add image and name to dialogue display

--- screens/dialogue_display.gd
+++ screens/dialogue_display.gd
@@ -3,6 +3,8 @@ extends Control
 
 var current_convo: Conversation
 var current_dialogue: Conversation.Dialogue
+var left_pos = Vector2(128, 364)
+var right_pos = Vector2(1536, 364)
 
 
 func _process(delta):
@@ -29,7 +31,15 @@ func next_dialogue():
 		end()
 		return
 	current_dialogue = current_convo.dialogues.pop_back()
+	$PicWindow/Header/Name.text = current_dialogue.name
+	$PicWindow/Pic.texture = current_dialogue.image
 	$ColorRect/Text.text = current_dialogue.text
+	
+	if current_dialogue.name == "Other Character":
+		$PicWindow.position = right_pos
+	else:
+		$PicWindow.position = left_pos
+	
 	$ColorRect/Text.visible_characters = 0
 	$ControlDelay.stop()
 	$ColorRect/ControlMessage.hide()
