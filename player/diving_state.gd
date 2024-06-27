2055af005ae5c60cd5bc077aef4af18f680c087a
Creativity Shark <94210249+CreativityShark@users.noreply.github.com>
2024-06-26 21:27:04 -0500

    Fix dive state not playing land SFX

--- player/diving_state.gd
+++ player/diving_state.gd
@@ -4,6 +4,7 @@ extends State
 
 func handle_input(player: Player, delta):
 	if player.is_on_floor():
+		player.get_node("LandSFX").play()
 		return player.STANDING_STATE
 	
 	return super(player, delta)
